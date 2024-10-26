c7dba6788b01605d0e37ba9c31658b7c7b8a3a37
Elise Walsh <ewal9011@uni.sydney.edu.au>
2024-10-26 19:54:06 +1100

    Added basic functionality to moving folders

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,14 +1,24 @@
 package folder
 
+import (
+	"strings"
+)
+
 // TODO: errors, error messages 
+// basic functionality: every path that has string, add dst before it (todo: other cases)
 func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	// (TODO: function description)
 
-	// basic functionality: every path that has string, add dst before it
-	// (todo: other cases)
 	// iterate through every folder path
-	// if it "Contains" (string function - import) a "name.", then add "string." prior to it
-	// (checking needs to be done before it)
+	for i := range f.Folders { // have to use i not _, otherwise modifying a copy
+		// split path into array, check where name is, add dst before, reconstruct path
+		pathLevels := strings.Split(f.Folders[i].Path, ".") // slice of path segments
+		for j, element := range pathLevels {
+			if element == name {
+				pathLevels[j] = dst + "." + element // making it dst.name in the slice
+				f.Folders[i].Path = strings.Join(pathLevels, ".") // put back together
+				// stop here?
+			}
 
 	return []Folder{}, nil
 }
